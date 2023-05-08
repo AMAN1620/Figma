09dc731ad072dca51dd3170e9fb6153a7c4525bc
Aman Kumar <aman.asingh307@gmail.com>
2023-05-08 15:08:18 +0530

    committed

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-const downloadBtn = document.getElementById('download-btn');
-downloadBtn.addEventListener('click', () => {
-    const { jsPDF } = window.jspdf;
-    const doc = new jsPDF();
-    const content = document.getElementById('content');
-    doc.html(content, {
-        callback: function () {
-            doc.save('document.pdf');
-        }
-    });
-});
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://www.google.com', { waitUntil: 'networkidle0' });
+
+  const pdf = await page.pdf({ format: 'A4' });
+
+  await browser.close();
+
+  // Write the PDF to a file
+  const fs = require('fs');
+  fs.writeFileSync('example.pdf', pdf);
+})();
+
+
+
+// const downloadBtn = document.getElementById('download-btn');
+// downloadBtn.addEventListener('click', () => {
+//     const { jsPDF } = window.jspdf;
+//     const doc = new jsPDF();
+//     const content = document.getElementById('content');
+//     doc.html(content, {
+//         callback: function () {
+//             doc.save('document.pdf');
+//         }
+//     });
+// });
 
 
 // function convertHTMLtoPDF() {
