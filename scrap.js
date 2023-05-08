b0edf11ab7697cef3be25e4b27cb7fec3c4ab268
Aman Kumar <aman.asingh307@gmail.com>
2023-05-08 17:41:02 +0530

    commit3

--- scrap.js
+++ scrap.js
@@ -1,40 +1,33 @@
 const puppeteer = require('puppeteer');
 
-async function downloadPDF(url) {
+async function downloadHtmlToPdf(pdfFilePath) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  // set the page size and margins
+  // Set the dimensions of the viewport to match the dimensions of the page.
   await page.setViewport({
-    width: 1200,
+    width: 1280,
     height: 800,
     deviceScaleFactor: 1,
   });
-  await page.emulateMediaType('print');
-  await page.evaluate(() => {
-    const style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = `@page {
-      margin: 1cm;
-    }`;
-    document.head.appendChild(style);
-  });
-
-  // navigate to the URL and wait for the page to load
-  await page.goto(url, { waitUntil: 'networkidle0' });
-
-  // trigger the print dialog using Ctrl+P
-  await page.keyboard.down('Control');
-  await page.keyboard.press('KeyP');
-  await page.keyboard.up('Control');
   
-  // wait for the print dialog to appear and press Enter to start the download
-  await page.waitForDevicePrompt(3000); // adjust this delay as needed
-  await page.keyboard.press('Enter');
-
-  // close the browser
+  // Navigate to the HTML file.
+  const url = "https://aman1620.github.io/Figma/";
+  await page.goto(url, { waitUntil: 'networkidle0' });
+  
+  // Wait for the page to load completely.
+  await page.waitForSelector('body');
+  
+  // Generate the PDF.
+  await page.pdf({
+    path: pdfFilePath,
+    format: 'A4',
+    printBackground: true,
+    margin: { top: '1cm', right: '1cm', bottom: '1cm', left: '1cm' },
+  });
+  
   await browser.close();
 }
 
-// call the function with the URL of the page you want to download as PDF
-downloadPDF('https://aman1620.github.io/Figma/');
+// Example usage:
+downloadHtmlToPdf('C:\Users\amana\OneDrive\Desktop\FigmaFageNew');
