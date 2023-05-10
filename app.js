dacdc1223512a7470402a11703eba0856faa1787
Aman Kumar <aman.asingh307@gmail.com>
2023-05-11 05:04:12 +0530

    till A4-8

--- app.js
+++ app.js
@@ -18,9 +18,7 @@ const puppeteer = require('puppeteer');
   // };
 
   const pdf = await page.pdf({ 
-    // format:'A4',
-    width: "297mm",
-    height: "210mm",
+    format:'A4',
     // margin : margin,
     printBackground: true,
     });
